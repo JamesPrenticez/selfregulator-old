441339108f681bdd1f57368d1e5f8bf3bcf634d6
James Prentice <66943799+JamesPrenticez@users.noreply.github.com>
2021-01-24 17:24:34 +1300

    Update TaskList.jsx

--- client/components/TaskList.jsx
+++ client/components/TaskList.jsx
@@ -8,7 +8,6 @@ import TaskListItem from './TaskListItem'
 
 class TaskList extends React.Component {
     componentDidMount() {
-        console.log('hi')
         getTasks()
         .then(tasks => {
             this.props.dispatch(setTasks(tasks))
