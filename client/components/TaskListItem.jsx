301212d61dca0839b429aa042d69507b33419c23
James-Prentice <Jamesprenticez@gmail.com>
2021-03-04 15:10:21 +1300

    Major break through

--- client/components/TaskListItem.jsx
+++ client/components/TaskListItem.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import {connect} from 'react-redux'
 
+import {FaMinusCircle} from 'react-icons/fa'
+
 import {removeTask} from '../actions'
 import {deleteTask} from '../api'
 
@@ -26,12 +28,21 @@ class TaskListItem extends React.Component {
         })
     }
 
-    changeColor = (i) => {
+    updateBoxesState = (i) => {
         let boxes = [...this.state.boxes]
         if(boxes[i] === 'neutral'){
             boxes[i] = 'pass'
             this.setState({
-                // boxes: event.target.value
+                boxes: boxes
+            })
+        }  else if(boxes[i] === 'pass'){
+            boxes[i] = 'fail'
+            this.setState({
+                boxes: boxes
+            })
+        } else if(boxes[i] === 'fail'){
+            boxes[i] = 'neutral'
+            this.setState({
                 boxes: boxes
             })
         }
@@ -45,6 +56,7 @@ class TaskListItem extends React.Component {
                 this.props.dispatch(updateBoxes(id, boxes))
                 if (this.props.onEscape) this.props.onEscape()
             })
+        console.log('submit success')
         }
 
     listenForKeys = (event) => {
@@ -57,15 +69,6 @@ class TaskListItem extends React.Component {
                 if (this.props.onEscape) this.props.onEscape()
                 break
         }
-    }
-
-    showControls = () => {
-        if (this.state.editing) return
-        this.setState({showControls: true})
-    }
-
-    hideControls = () => {
-        this.setState({showControls: false})
     }
 
     deleteTask = () => {
@@ -92,52 +95,70 @@ class TaskListItem extends React.Component {
 
     render(){
     const {task} = this.props
-    const {showControls, editing} = this.state
-    const editStyle = {color: 'orange', marginLeft: '7px', cursor: 'pointer'}
+    const {editing} = this.state
     const deleteStyle = {color: 'red', marginLeft: '7px', cursor: 'pointer'}
         return(
             <div
-                // onMouseEnter={this.showControls}
-                // onMouseLeave={this.hideControls}
-                // style={{height: '45px'}}
+                style={{height: '45px'}}
             >
-                {/* { editing ? <TaskEdit task={task} onEscape={this.hideEditForm}/> : task.name + ' ' + task.boxes} */}
-
                 { editing ? <TaskEdit task={task} onEscape={this.hideEditForm}/> :
-                <>
-                <div className='taskName' onClick={this.editTask}>{task.name}</div>
 
+                <>
+                <div className='taskName' onClick={this.editTask}>{this.state.name}</div>
+
+                {/*Box 0*/}
                 <input 
                     className='box'
                     name='name'
                     id={0}
                     value={this.state.boxes[0]}
-                    onChange={this.handleChange}
-                    onClick={() => this.changeColor(0)}
+                    onChange={() => this.handleChange()}
+                    onClick={() => this.updateBoxesState(0)}
                 />
-<button onClick={()=> this.submit()}>Submit</button>
+                {/*Box 1*/}
+                <input 
+                    className='box'
+                    name='name'
+                    id={1}
+                    value={this.state.boxes[1]}
+                    onChange={() => this.handleChange()}
+                    onClick={() => this.updateBoxesState(1)}
+                />
+                {/*Box 2*/}
+                <input 
+                    className='box'
+                    name='name'
+                    id={2}
+                    value={this.state.boxes[2]}
+                    onChange={() => this.handleChange()}
+                    onClick={() => this.updateBoxesState(2)}
+                />
+                {/*Box 3*/}
+                <input 
+                    className='box'
+                    name='name'
+                    id={3}
+                    value={this.state.boxes[3]}
+                    onChange={() => this.handleChange()}
+                    onClick={() => this.updateBoxesState(3)}
+                />
+                {/*Box 4*/}
+                <input 
+                    className='box'
+                    name='name'
+                    id={4}
+                    value={this.state.boxes[4]}
+                    onChange={() => this.handleChange()}
+                    onClick={() => this.updateBoxesState(4)}
+                />
 
-{/* 
-                        <div className='taskName' onClick={this.editTask}>{task.name}</div>
-                        <input type='button' className='box' id={0} name='name' value={this.state.boxes[0]} onChange={this.handleChange}>{task.boxes[0]}</input>
-                        <input>fuckyou</input>
-                        <div className='box' id={1} >{task.boxes[1]}</div>
-                        <div className='box' id={2} >{task.boxes[2]}</div>
-                        <div className='box' id={3} >{task.boxes[3]}</div>
-                        <div className='box' id={4} >{task.boxes[4]}</div>
+                <button onClick={() => this.submit()}>submit</button>
 
-                    { showControls ? <>
-                        <FaEdit
-                            style={editStyle}
-                            onClick={this.editTask}
-                            role='button'
-                        />
-                        <FaMinusCircle
-                            style={deleteStyle}
-                            onClick={this.deleteTask}
-                            role='button'
-                        />
-                    </> : ''} */}
+                <FaMinusCircle
+                    style={deleteStyle}
+                    onClick={() => this.deleteTask()}
+                    role='button'
+                />
 
                 </>
                 }
